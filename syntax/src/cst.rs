ba098caf4aece6009f45b7a4bacb8f9dddb0e68d
tarberd <bernardo.mferrari@gmail.com>
2021-03-09 23:18:33 -0300

    Add basic typechecking with expressions, literal, and function

--- syntax/src/cst.rs
+++ syntax/src/cst.rs
@@ -8,6 +8,8 @@ use raw::SyntaxNode;
 use std::convert::TryFrom;
 use std::fmt::Display;
 
+use self::{expression::Expression, function::Function, source_file::SourceFile};
+
 pub trait CstNode: TryFrom<SyntaxNode, Error = SyntaxToAstError> + Display {
     fn as_syntax_node(&self) -> &SyntaxNode;
 }
@@ -21,4 +23,14 @@ impl SyntaxToAstError {
     pub fn new(expected: SyntaxKind, found: SyntaxKind) -> Self {
         Self { expected, found }
     }
+}
+
+// TODO: please let's change this, the only reason this exists
+// is how are we going to match the types(implementors) of a trait in the HIR lower method
+// https://stackoverflow.com/questions/26126683/how-to-match-trait-implementors
+#[derive(Debug)]
+pub enum CstNodeEnum {
+    Function(Function),
+    Expression(Expression),
+    SourceFile(SourceFile),
 }
