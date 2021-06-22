67aae23588cbe858be6275f726d03ddd59b90ae5
tarberd <bernardo.mferrari@gmail.com>
2021-06-22 18:33:31 -0300

    hir arena draft

--- hir/src/lib.rs
+++ hir/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod type_check;
 
+use la_arena::{Arena, Idx};
 use smol_str::SmolStr;
 use syntax::cst;
 
@@ -14,26 +15,26 @@ pub struct Module {
     pub functions: Vec<Function>,
 }
 
-impl Module {
-    pub fn lower(source: cst::SourceFile) -> (Self, Vec<LowerError>) {
-        let lowered = source.functions().fold(
-            (Vec::new(), Vec::new()),
-            |(mut functions, diagnostics), function| {
-                let lowered = Function::lower(function, diagnostics);
-                if let Some(fun) = lowered.0 {
-                    functions.push(fun);
-                }
-                (functions, lowered.1)
-            },
-        );
-        (
-            Self {
-                functions: lowered.0,
-            },
-            lowered.1,
-        )
-    }
-}
+// impl Module {
+//     pub fn lower(source: cst::SourceFile) -> (Self, Vec<LowerError>) {
+//         let lowered = source.functions().fold(
+//             (Vec::new(), Vec::new()),
+//             |(mut functions, diagnostics), function| {
+//                 let lowered = Function::lower(function, diagnostics);
+//                 if let Some(fun) = lowered.0 {
+//                     functions.push(fun);
+//                 }
+//                 (functions, lowered.1)
+//             },
+//         );
+//         (
+//             Self {
+//                 functions: lowered.0,
+//             },
+//             lowered.1,
+//         )
+//     }
+// }
 
 #[derive(Debug)]
 pub struct Parameter {
@@ -49,40 +50,193 @@ pub struct Function {
     pub body: Body,
 }
 
-impl Function {
-    fn lower(f: cst::Function, diagnostics: Vec<LowerError>) -> (Option<Self>, Vec<LowerError>) {
-        let parameters = {
-            if let Some(param_list) = f.parameter_list() {
-                let params = param_list.parameters();
-                params
-                    .filter_map(|param| match (param.name(), param.type_name()) {
-                        (Some(n), Some(t)) => Some(Parameter {
-                            name: n.identifier().text().into(),
-                            ty: t.identifier().text().into(),
-                        }),
-                        (_, _) => None,
-                    })
-                    .collect()
-            } else {
-                Vec::new()
+// impl Function {
+//     fn lower(f: cst::Function, diagnostics: Vec<LowerError>) -> (Option<Self>, Vec<LowerError>) {
+//         let parameters = {
+//             if let Some(param_list) = f.parameter_list() {
+//                 let params = param_list.parameters();
+//                 params
+//                     .filter_map(|param| match (param.name(), param.type_name()) {
+//                         (Some(n), Some(t)) => Some(Parameter {
+//                             name: n.identifier().text().into(),
+//                             ty: t.identifier().text().into(),
+//                         }),
+//                         (_, _) => None,
+//                     })
+//                     .collect()
+//             } else {
+//                 Vec::new()
+//             }
+//         };
+
+//         let parameter_types = parameters
+//             .iter()
+//             .map(|Parameter { ty, .. }| ty.clone())
+//             .collect();
+//         let (block, diagnostics) = BlockExpression::lower(f.body().unwrap(), diagnostics);
+//         let fun = match f.name() {
+//             Some(name) => Some(Self {
+//                 name: name.identifier().text().into(),
+//                 parameter_types,
+//                 return_type: "i32".into(),
+//                 body: Body { parameters, block },
+//             }),
+//             None => None,
+//         };
+//         (fun, diagnostics)
+//     }
+// }
+
+#[derive(Debug)]
+pub struct Body {
+    pub parameters: Vec<Parameter>,
+    pub expressions: Arena<Expression>,
+    pub block: ExpressionId,
+}
+
+impl Body {
+    pub fn new(parameters: Vec<Parameter>, block: cst::BlockExpression) -> Self {
+        let expressions = Arena::new();
+        let (expressions, block) = Self::collect_block_expression(expressions, block);
+        Self {
+            parameters,
+            expressions,
+            block,
+        }
+    }
+
+    fn collect_block_expression(
+        expressions: Arena<Expression>,
+        block: cst::BlockExpression,
+    ) -> (Arena<Expression>, ExpressionId) {
+        match block.tail_expression() {
+            Some(tail) => {
+                let (expressions, tail) = Self::collect_expression(expressions, tail);
+                let block = BlockExpression {
+                    tail_expression: tail,
+                };
+                let expr = Expression::BlockExpression(block);
+                let id = expressions.alloc(expr);
+                (expressions, id)
             }
+            None => panic!("missing tail expression from block"),
+        }
+    }
+
+    fn collect_expression(
+        expressions: Arena<Expression>,
+        expression: cst::Expression,
+    ) -> (Arena<Expression>, ExpressionId) {
+        match expression {
+            cst::Expression::BlockExpression(block) => {
+                Self::collect_block_expression(expressions, block)
+            }
+            cst::Expression::Literal(lit) => {
+                let (lit, _) = Literal::lower(lit, vec![]);
+                let literal = Expression::Literal(lit);
+                let id = expressions.alloc(literal);
+                (expressions, id)
+            }
+            cst::Expression::NameReference(name_ref) => {
+                let name_ref = Expression::NameReference(name_ref.identifier().text().into());
+                let id = expressions.alloc(name_ref);
+                (expressions, id)
+            }
+            cst::Expression::InfixExpression(infix) => {
+                Self::collect_infix_expression(expressions, infix)
+            }
+            cst::Expression::PrefixExpression(prefix) => {
+                Self::collect_prefix_expression(expressions, prefix)
+            }
+            cst::Expression::ParenthesisExpression(inner) => Self::collect_expression(
+                expressions,
+                inner
+                    .inner_expression()
+                    .expect("missing expression from Parenthesis Expression"),
+            ),
+            cst::Expression::CallExpression(call) => {
+                Self::collect_call_expression(expressions, call)
+            }
+        }
+    }
+
+    fn collect_infix_expression(
+        expressions: Arena<Expression>,
+        infix: cst::InfixExpression,
+    ) -> (Arena<Expression>, ExpressionId) {
+        let (expressions, lhs) = Self::collect_expression(
+            expressions,
+            infix.lhs().expect("missing lhs from infix expression"),
+        );
+        let (expressions, rhs) = Self::collect_expression(
+            expressions,
+            infix.rhs().expect("missing rhs from infix expression"),
+        );
+
+        let op = match infix
+            .operator()
+            .expect("missing operator from infix expression")
+        {
+            cst::BinaryOperator::Asterisk(_) => BinaryOperator::Mul,
+            cst::BinaryOperator::Plus(_) => BinaryOperator::Add,
+            cst::BinaryOperator::Minus(_) => BinaryOperator::Sub,
+            cst::BinaryOperator::Slash(_) => BinaryOperator::Div,
+            cst::BinaryOperator::Percent(_) => BinaryOperator::Rem,
         };
 
-        let parameter_types = parameters
-            .iter()
-            .map(|Parameter { ty, .. }| ty.clone())
-            .collect();
-        let (block, diagnostics) = BlockExpression::lower(f.body().unwrap(), diagnostics);
-        let fun = match f.name() {
-            Some(name) => Some(Self {
-                name: name.identifier().text().into(),
-                parameter_types,
-                return_type: "i32".into(),
-                body: Body { parameters, block },
-            }),
-            None => None,
+        let bin_expr = Expression::BinaryExpression(op, lhs, rhs);
+        let id = expressions.alloc(bin_expr);
+        (expressions, id)
+    }
+
+    fn collect_prefix_expression(
+        expressions: Arena<Expression>,
+        prefix: cst::PrefixExpression,
+    ) -> (Arena<Expression>, ExpressionId) {
+        let (expressions, inner) = Self::collect_expression(
+            expressions,
+            prefix
+                .inner()
+                .expect("missing inner expression from prefix expression"),
+        );
+
+        let op = match prefix
+            .operator()
+            .expect("missing operator from infix expression")
+        {
+            cst::UnaryOperator::Minus(_) => UnaryOperator::Minus,
         };
-        (fun, diagnostics)
+
+        let unary_expr = Expression::UnaryExpression(op, inner);
+        let id = expressions.alloc(unary_expr);
+        (expressions, id)
+    }
+
+    fn collect_call_expression(
+        expressions: Arena<Expression>,
+        call: cst::CallExpression,
+    ) -> (Arena<Expression>, ExpressionId) {
+        let (expressions, callee) = Self::collect_expression(
+            expressions,
+            call.callee().expect("missing callee from call expression"),
+        );
+
+        let (expression, arguments) = call
+            .argument_list()
+            .expect("missing argument list from call expression")
+            .arguments()
+            .fold(
+                (expressions, Vec::new()),
+                |(expressions, arguments), expression| {
+                    let (expressions, argument) = Self::collect_expression(expressions, expression);
+                    arguments.push(argument);
+                    (expressions, arguments)
+                },
+            );
+
+        let call = Expression::Call(Call { callee, arguments });
+        let id = expressions.alloc(call);
+        (expressions, id)
     }
 }
 
@@ -100,14 +254,16 @@ pub enum UnaryOperator {
     Minus,
 }
 
+pub type ExpressionId = Idx<Expression>;
+
 #[derive(Debug)]
 pub enum Expression {
-    BlockExpression(Box<BlockExpression>),
-    BinaryExpression(Box<Expression>, BinaryOperator, Box<Expression>),
-    UnaryExpression(Box<Expression>, UnaryOperator),
+    BlockExpression(BlockExpression),
+    BinaryExpression(BinaryOperator, ExpressionId, ExpressionId),
+    UnaryExpression(UnaryOperator, ExpressionId),
     Literal(Literal),
     NameReference(SmolStr),
-    Call(Box<Call>),
+    Call(Call),
 }
 
 impl Expression {
@@ -189,8 +345,8 @@ impl Expression {
 
 #[derive(Debug)]
 pub struct Call {
-    pub callee: Expression,
-    pub arguments: Vec<Expression>,
+    pub callee: ExpressionId,
+    pub arguments: Vec<ExpressionId>,
 }
 
 impl Call {
@@ -215,14 +371,8 @@ impl Call {
 }
 
 #[derive(Debug)]
-pub struct Body {
-    pub parameters: Vec<Parameter>,
-    pub block: BlockExpression,
-}
-
-#[derive(Debug)]
 pub struct BlockExpression {
-    pub tail_expression: Expression,
+    pub tail_expression: ExpressionId,
 }
 
 impl BlockExpression {
